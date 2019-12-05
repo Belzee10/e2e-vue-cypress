1359b91546a46992a0d032d27a7b4df870491f80
Ivan <belzee92@gmail.com>
2019-12-05 14:44:47 -0500

    add some deps

---
+++ src/api/index.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const getArticles = async () => {
+  try {
+    const res = await axios.get("http://localhost:3100/articles");
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const editArticle = async (id, data) => {
+  try {
+    const res = await axios.put(`http://localhost:3100/articles/${id}`, data);
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
